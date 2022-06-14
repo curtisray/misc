8a2be3af7f7e5422c1416fa7e05ffef6bb97992e
curtisray <curtisraymaloney@gmail.com>
2022-06-14 11:49:07 -0700

    init -- ALPHA

---
+++ c/circle/area.c
@@ -0,0 +1,18 @@
+// print the area of a circle
+
+#include <stdio.h>
+
+int main()
+{
+	// declar input and output variables
+	int radius;
+	int area;
+	
+	// get user input
+	printf("Radius Length: ");
+	scanf("%i", &radius);
+
+	// print the output
+	printf("%i\n", area);
+	return 0;
+}
---
+++ c/hello/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	// basic printf
+	printf("Hello World!\n");
+
+	// print int type
+	int hello;
+	hello = 10;
+
+	int hello_world = 10/2;
+
+	printf("%i : %i\n", hello, hello_world);
+	printf("The first number was - %i\nand the second was - %i\n", hello, hello_world); 
+	
+	// return nothing
+	return 0;
+}
